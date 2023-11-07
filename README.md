b232f95812fd8f0bae650ed50ae2e0768ce10c8b
martyna-gg <martyna.gardzinska@gmail.com>
2023-11-07 12:04:54 +0100

    creating readme, addingclosing after no answer in menu

--- README.md
+++ README.md
@@ -1,3 +1,73 @@
 # Duplicate_File_Handler
 
-A program for detecting and removing duplicate files with usage of os.walk and hashlib tools
+A program for detecting and removing duplicate files with usage of os, sys and hashlib modules, lists, dicts, if/else statements, while and for loops and processing user input.
+
+## Installation
+
+Os, sys and hashlib modules are python built-in modules. No installation is required.
+
+## Requirements
+
+Code was developed and tested using: 
+
+Ubuntu 18.04.6 LTS 
+
+Python 3.6.9
+
+## Usage
+
+While running a program from the terminal, please pass the directory you want to check for duplicates as an argument.
+```
+$ python3 file_handler.py /home/martyna/Downloads
+
+```
+Then program will ask you to define the format of files you want to check (if you want to check all the formats just press Enter). Next you need to decide if you want the files to be listed in ascending or descending order. Afterwards a list of all files of the same size will be printed.
+```
+$ python3 file_handler.py /home/martyna/Downloads
+Enter file format: >jpg
+Size sorting options:
+1. Descending
+2. Ascending
+> 1
+607681 bytes
+/home/martyna/Downloads/hdimged38f3776e673701a9341887dee1cb (copy).jpg
+/home/martyna/Downloads/hdimged38f3776e673701a9341887dee1cb.jpg
+
+413270 bytes
+/home/martyna/Downloads/hdimg27036eb627db468c3e11ff29badaa8 (another copy).jpg
+/home/martyna/Downloads/hdimg27036eb627db468c3e11ff29badaa8 (copy).jpg
+/home/martyna/Downloads/hdimg27036eb627db468c3e11ff29badaa8.jpg
+
+```
+In the next step the program will ask you if you want to check the files of the same size for duplicates among them. If you insert yes the list of duplicates will be printed. Otherwise the program will stop running.
+
+```
+Check for duplicates? >yes
+607681 bytes
+Hash: 7dec62fe10fc80569034b6ee6c7e6e19
+1. /home/martyna/Downloads/hdimged38f3776e673701a9341887dee1cb (copy).jpg
+2. /home/martyna/Downloads/hdimged38f3776e673701a9341887dee1cb.jpg
+
+413270 bytes
+Hash: 95931ba28bd7873997f5a68dfc60bbe2
+3. /home/martyna/Downloads/hdimg27036eb627db468c3e11ff29badaa8  (copy).jpg
+4. /home/martyna/Downloads/hdimg27036eb627db468c3e11ff29badaa8  (another copy).jpg
+5. /home/martyna/Downloads/hdimg27036eb627db468c3e11ff29badaa8 .jpg
+
+```
+
+After that you need to decide if you want to delete the duplicates. If you do, you need to specify which files should be deleted by inserting numbers of the files in the list (separated by white space). When you do so, the files will be deleted and the information about freed up space will be printed. If you won't decide to delete any files the program will stop running.
+
+```
+Delete files? >yes
+Enter file numbers to delete: >1 3 4
+Total freed up space:1434221 bytes
+
+```
+
+## Support
+
+If you face any problem let me know by Issue.
+
+
+
--- file_handler.py
+++ file_handler.py
@@ -6,15 +6,17 @@ args = sys.argv  # take directory as an argument
 if len(args) == 1:
     print("Directory is not specified")
 else:
-    decision1 = input('Enter file format:')  # specify a file format
+    decision1 = input('Enter file format: >')  # specify a file format
     decision2 = input('''Size sorting options:
 1. Descending
-2. Ascending''')  # specify type of sorting
+2. Ascending
+> ''')  # specify type of sorting
     while decision2 not in ['1', '2']:
         print('Wrong option')
         decision2 = input('''Size sorting options:
 1. Descending
-2. Ascending''')
+2. Ascending
+> ''')
     files_sizes = {}
     for root, dirs, files in os.walk(args[1], topdown=True):  # create a dict with files and their sizes
         for name in files:
@@ -23,7 +25,7 @@ else:
                     files_sizes[os.path.getsize(os.path.join(root, name))].append(os.path.join(root, name))
                 else:
                     files_sizes[(os.path.getsize(os.path.join(root, name)))] = [os.path.join(root, name)]
-    # create a dict with files od the same sizes with sizes as keys
+    # create a dict with files of the same sizes with sizes as keys
     files_same_size = {key: value for (key, value) in files_sizes.items() if len(value) > 1}
     files_sizes =  list(files_same_size.keys())
     if decision2 == '1':
@@ -39,12 +41,12 @@ else:
             print(x)
         print()
 
-    decision3 = input('Check for duplicates?')  # decision if checking for duplicates
+    decision3 = input('Check for duplicates? >')  # decision if checking for duplicates
     while decision3 not in ['yes', 'no']:
         print('Wrong option')
-        decision3 = input('Check for duplicates?')
+        decision3 = input('Check for duplicates? >')
     if decision3 == 'yes':
-        files_hash = {key: {} for key in files_same_size}  # create a dict with files of same sizes with hashes
+        files_hash = {key: {} for key in files_same_size}  # create a dict with files of the same sizes with hashes
         for key in files_same_size:
             for x in files_same_size[key]:
                 with open(x, 'rb') as x_file:
@@ -54,48 +56,48 @@ else:
                     else:
                         files_hash[key][hash_x] = [x]
 
-    files_same_hash = {key: {} for key in files_hash}  # creates a dict with files of same size and same hash
-    for size in files_hash:
-        for hash_v in files_hash[size]:
-            if len(files_hash[size][hash_v]) > 1:
-                files_same_hash[size].update({hash_v: files_hash[size][hash_v]})
-                
-    counter = 1  # prints list of files of same size and same hash
-    for size in files_same_hash:
-        print(str(size) + ' bytes')
-        for hash_v in files_same_hash[size]:
-            print('Hash: ' + str(hash_v))
-            for x in files_same_hash[size][hash_v]:
-                x = str(counter) + '. ' + x
-                counter += 1
-                print(x)
-        print()
-
-    decision4 = input('Delete files?') # decision of deleting duplicates
-    while decision4 not in ['yes', 'no']:
-        print('Wrong option')
-        decision4 = input('Check for duplicates?')
-    if decision4 == 'yes':
-
-        decision5 = input('Enter file numbers to delete:')  # decision which files to delete
-        try:
-            while not (decision5 != '' and all([int(x) in range(1, counter+1) for x in decision5.split(' ')])):
-                print("Wrong format")
-                decision5 = input('Give me numbers')
-        except:
-            print("Wrong format")
-            decision5 = input('Give me numbers')
-
-        files_del = [int(x) for x in decision5.split(' ')]  # deleting files
-        freed_space = 0
-        counter = 0
+        files_same_hash = {key: {} for key in files_hash}  # create a dict with files of same size and same hash
+        for size in files_hash:
+            for hash_v in files_hash[size]:
+                if len(files_hash[size][hash_v]) > 1:
+                    files_same_hash[size].update({hash_v: files_hash[size][hash_v]})
+                    
+        counter = 1  # prints list of files of same size and same hash
         for size in files_same_hash:
+            print(str(size) + ' bytes')
             for hash_v in files_same_hash[size]:
+                print('Hash: ' + str(hash_v))
                 for x in files_same_hash[size][hash_v]:
+                    x = str(counter) + '. ' + x
                     counter += 1
-                    if counter in files_del:
-                        freed_space += int(size)
-                        os.remove(x)
+                    print(x)
+            print()
 
-        print('Total freed up space:' + str(freed_space) + ' bytes')
+        decision4 = input('Delete files? >') # decision of deleting duplicates
+        while decision4 not in ['yes', 'no']:
+            print('Wrong option')
+            decision4 = input('Check for duplicates? >')
+        if decision4 == 'yes':
+
+            decision5 = input('Enter file numbers to delete: >')  # decision which files to delete
+            try:
+                while not (decision5 != '' and all([int(x) in range(1, counter+1) for x in decision5.split(' ')])):
+                    print("Wrong format")
+                    decision5 = input('Enter file numbers to delete: >')
+            except:
+                print("Wrong format")
+                decision5 = input('Enter file numbers to delete: >')
+
+            files_del = [int(x) for x in decision5.split(' ')]  # deleting files
+            freed_space = 0
+            counter = 0
+            for size in files_same_hash:
+                for hash_v in files_same_hash[size]:
+                    for x in files_same_hash[size][hash_v]:
+                        counter += 1
+                        if counter in files_del:
+                            freed_space += int(size)
+                            os.remove(x)
+
+            print('Total freed up space:' + str(freed_space) + ' bytes')
 
